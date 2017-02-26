7627fc6fdabfb292ed39f278e331d242106e81df
Huat Zhi <huat@hz.WirelessAP>
2017-02-26 05:19:06 +0000

    build dummy user routes

--- app.js
+++ app.js
@@ -7,8 +7,25 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const users = require('./routes/users');
+
+//Port Number
 const port = 3000;
 
+// CORS Middleware
+app.use(cors());
+
+app.use('/users',users);
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+//Index Route
+app.get('/', (req,res) => {
+  res.send('Invalid Endpoint');
+});
+
+//Start Server
 app.listen(port, () => {
   console.log('Server started on port ' + port );
 });
